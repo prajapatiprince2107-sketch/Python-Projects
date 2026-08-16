a491b065e99d3222627014325c83f2ab5407cd0b
Prince Prajapati <prajapatiprince2107@gmail.com>
2026-08-16 11:14:50 +0530

    02

---
+++ 02.py
@@ -0,0 +1,10 @@
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+course = input("Enter your course: ")
+
+future_age = age + 5
+
+print(f"Your name is {name}")
+print(f"Your age is {age}")
+print(f"After 5 years, your age will be {future_age}")
+print(f"Your course is {course}")
