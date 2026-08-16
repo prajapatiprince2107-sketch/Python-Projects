4450bbbecb64b0d3e9eadfe0599b30663e4c6c19
Prince Prajapati <prajapatiprince2107@gmail.com>
2026-08-16 11:22:13 +0530

    Remove old practice files

--- 01_Basics/03.py
+++
@@ -1,9 +0,0 @@
-a = 20
-b = 6
-
-print("Addition:", a + b)
-print("Subtraction:", a - b)
-print("Multiplication:", a * b)
-print("Division:", a / b)
-print("Remainder:", a % b)
-print("Floor Division:", a // b)
