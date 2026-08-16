828bcac001533ed759faf935192e40378b167e2e
Prince Prajapati <prajapatiprince2107@gmail.com>
2026-08-16 11:35:46 +0530

    02

---
+++ 01_Basics/operators.py/02.py
@@ -0,0 +1,9 @@
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+print("Addition:", a + b)
+print("Subtraction:", a - b)
+print("Multiplication:", a * b)
+print("Division:", a / b)
+print("Remainder:", a % b)
+print("Floor Division:", a // b)
