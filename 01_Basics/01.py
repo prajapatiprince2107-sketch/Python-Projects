9c3576d5284e8149bb0f010318411349f5529138
Prince Prajapati <prajapatiprince2107@gmail.com>
2026-08-16 11:11:09 +0530

    01

---
+++ 01_Basics/01.py
@@ -0,0 +1,5 @@
+Enter_your_name =input("Enter ur name: ")
+Enter_your_age = input("Enter ur age: ")
+Enter_your_course = input("Enter ur course: ")
+
+print(f"Your name is {Enter_your_name} and Your age is {Enter_your_age} and Your course is {Enter_your_course}")
