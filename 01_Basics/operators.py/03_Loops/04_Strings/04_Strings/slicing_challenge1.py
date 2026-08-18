a446c6bff609682f3d1261c0eaaf665067e13f2d
Prince Prajapati <prajapatiprince2107@gmail.com>
2026-08-19 03:38:06 +0530

    Add while loop sum challenge

---
+++ 01_Basics/operators.py/03_Loops/04_Strings/04_Strings/slicing_challenge1.py
@@ -0,0 +1,10 @@
+word = input("Enter a word: ")
+
+# First 3 characters print karo
+print("First 3:", word[:3])
+
+# Last 3 characters print karo
+print("Last 3:", word[-3:])
+
+# Word ne reverse karo
+print("Reverse:", word[::-1])
