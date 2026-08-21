d58027d9bf41e8a58710ab6187663283dfa211b7
Prince Prajapati <prajapatiprince2107@gmail.com>
2026-08-21 09:00:06 +0530

    username_analyzer.py

---
+++ 01_Basics/operators.py/03_Loops/04_Strings/04_Strings/Projects/username_analyzer.py
@@ -0,0 +1,8 @@
+username = input("Enter your username: ")
+
+print("Uppercase:", username.upper())
+print("Lowercase:", username.lower())
+print("Total characters:", len(username))
+print("First character:", username[0])
+print("Last character:", username[-1])
+print("Reverse:", username[::-1])
