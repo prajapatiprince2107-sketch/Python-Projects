d3b3f26fb9ddb5a64a50c1d394a446d0e10fb6aa
Prince Prajapati <prajapatiprince2107@gmail.com>
2026-08-22 08:15:53 +0530

    revers

--- 01_Basics/operators.py/03_Loops/revers.py
+++ 01_Basics/operators.py/03_Loops/revers.py
@@ -1,6 +1,9 @@
 revers = int(input("Enter ur number: "))
+reverse = 0
 
 while revers > 0:
     digit = revers % 10
-    print(digit)
+    reverse = reverse * 10 + digit
     revers = revers // 10
+
+print(reverse)
