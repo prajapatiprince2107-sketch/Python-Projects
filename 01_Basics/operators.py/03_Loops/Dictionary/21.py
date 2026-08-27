806cf6075e44d93fe727fbe071d5047e36bf0178
Prince Prajapati <prajapatiprince2107@gmail.com>
2026-08-28 05:29:53 +0530

    nested

---
+++ 01_Basics/operators.py/03_Loops/Dictionary/21.py
@@ -0,0 +1,12 @@
+students = {
+    "student1": {
+        "name": "Rahul",
+        "age": 20
+    },
+    "student2": {
+        "name": "Amit",
+        "age": 22
+    }
+}
+
+print(students["student1"]["age"])
