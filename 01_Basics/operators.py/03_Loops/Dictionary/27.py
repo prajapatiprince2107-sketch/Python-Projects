ddfff90b974996a254bd567983f48bf3cc1cf9e4
Prince Prajapati <prajapatiprince2107@gmail.com>
2026-08-29 19:17:06 +0530

    27.py

---
+++ 01_Basics/operators.py/03_Loops/Dictionary/27.py
@@ -0,0 +1,9 @@
+students = {
+    "student1": {"name": "Rahul", "age": 21, "city": "Ahmedabad"},
+    "student2": {"name": "Prince", "age": 22, "city": "Surat"},
+    "student3": {"name": "Amit", "age": 25, "city": "Rajkot"}
+}
+
+for key,value in students.items():
+    if value["age"] > 21:
+        print(value["name"],value["city"])
