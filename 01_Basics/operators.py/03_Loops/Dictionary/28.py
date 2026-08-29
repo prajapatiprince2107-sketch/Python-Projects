34f08316b175642e5826bef03326263d17ce79a1
Prince Prajapati <prajapatiprince2107@gmail.com>
2026-08-29 19:22:31 +0530

    28.py

---
+++ 01_Basics/operators.py/03_Loops/Dictionary/28.py
@@ -0,0 +1,10 @@
+students = {
+    "student1": {"name": "Rahul", "age": 17, "city": "Ahmedabad"},
+    "student2": {"name": "Prince", "age": 22, "city": "Surat"},
+    "student3": {"name": "Amit", "age": 19, "city": "Rajkot"}
+}
+for key,value in students.items():
+    if value["age"] >= 18 :
+        print(value["name"],"Adult")
+    else:
+        print(value["name"],"Minor",value)
