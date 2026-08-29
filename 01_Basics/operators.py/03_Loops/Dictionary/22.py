62580c0d6a301959041e969189eb6abca83e0dff
Prince Prajapati <prajapatiprince2107@gmail.com>
2026-08-29 18:25:54 +0530

    nested

---
+++ 01_Basics/operators.py/03_Loops/Dictionary/22.py
@@ -0,0 +1,12 @@
+students = {
+    "student1": {
+        "name": "Rahul",
+        "age": 20,
+        "city": "Ahmedabad"
+    },
+    "student2": {
+        "name": "Prince",
+        "age": 22,
+        "city": "Surat"
+    }
+}
