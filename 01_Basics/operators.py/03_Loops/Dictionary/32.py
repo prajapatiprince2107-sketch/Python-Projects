e8c406db1dd64e4a655676732cab09bec2127d19
Prince Prajapati <prajapatiprince2107@gmail.com>
2026-08-30 23:11:38 +0530

    32.py

---
+++ 01_Basics/operators.py/03_Loops/Dictionary/32.py
@@ -0,0 +1,11 @@
+students = {
+    "student1": {"name": "Prince", "age": 22},
+    "student2": {"name": "Rahul", "age": 20}
+    
+}   
+students["student3"] = {
+    "name": "Jay",
+    "age": 21
+}
+print(students["student3"]["name"])
+  
