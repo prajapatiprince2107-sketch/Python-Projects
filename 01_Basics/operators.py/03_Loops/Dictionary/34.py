5f17cb4ad49ab40d3be4c4ae1198fc28353c701f
Prince Prajapati <prajapatiprince2107@gmail.com>
2026-08-30 23:13:37 +0530

    34.py

---
+++ 01_Basics/operators.py/03_Loops/Dictionary/34.py
@@ -0,0 +1,7 @@
+students = {
+    "student1": {"name": "Prince", "age": 22},
+    "student2": {"name": "Amit", "age": 25}
+}
+students["student2"]["city"] = "Ahmedabad"
+
+print(students["student2"])
